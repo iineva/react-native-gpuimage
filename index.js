63004f26db3b15d295c5c1b2ace90edef32f549d
Steven <ineva@qq.com>
2017-07-01 19:43:30 +0800

    Add propTypes

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+import React from 'react'
+import {View} from 'react-native'
+import propTypes from 'prop-types'
 import { requireNativeComponent } from 'react-native'
 
-export const GPUCameraView = requireNativeComponent('RCTGPUCameraView')
+const RCTGPUCameraView = requireNativeComponent('RCTGPUCameraView')
+
+export class GPUCameraView extends React.Component {
+
+  static propTypes = {
+    style: View.propTypes.style,
+    mirror: PropTypes.bool,
+    capture: PropTypes.bool,
+    cameraPosition: PropTypes.oneOf(['front', 'back']),
+    filters: PropTypes.oneOfTypes([
+      PropTypes.string,
+      PropTypes.shape({
+        name: PropTypes.string,
+        param: PropTypes.object,
+      })
+    ])
+  }
+
+  static defaultProps = {
+    capture: true,
+  }
+
+  render = ()=>(
+    <RCTGPUCameraView {...this.props}/>
+  )
+}
+
